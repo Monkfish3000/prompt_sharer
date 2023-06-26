7c87a2baa2c06f8420dba28803132b93efc6259a
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-06-26 15:14:04 +0200

    title, sub-title and description added and styled on home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,19 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="min-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Searching for AI-Prompts has never been easier. Save, search and share
+        your favourite AI prompts all in once place.{' '}
+      </p>
+
+      {/* Feed component */}
+    </section>
+  );
 };
 
 export default Home;
