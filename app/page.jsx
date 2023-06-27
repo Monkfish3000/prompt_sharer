c27a396c87abf98d3f516e14d6d969f566370dca
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-06-27 10:59:32 +0200

    navbar added

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from '@components/Feed';
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -11,7 +13,7 @@ const Home = () => {
         your favourite AI prompts all in once place.{' '}
       </p>
 
-      {/* Feed component */}
+      <Feed />
     </section>
   );
 };
