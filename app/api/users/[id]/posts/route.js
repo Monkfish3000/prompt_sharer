245f82512282782419729324314209c6753f2bb8
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-07-06 09:04:37 +0200

    added params to get individual id to GET prompts of one user

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -1,11 +1,15 @@
 import { connectToDB } from '@utils/database';
 import Prompt from '@models/prompt';
 
-export const GET = async (req) => {
+export const GET = async (req, { params }) => {
   try {
     await connectToDB();
 
-    const prompts = await Prompt.find({}).populate('creator');
+    params.id;
+
+    const prompts = await Prompt.find({
+      creator: params.id,
+    }).populate('creator');
 
     return new Response(JSON.stringify(prompts), {
       status: 200,
