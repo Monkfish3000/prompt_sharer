121798f78e59d7fea4680cecad8bbcbf621a5b1d
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-07-11 12:15:00 +0200

    PATCH endpoint for prompt API

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -21,5 +21,26 @@ export const GET = async (req, { params }) => {
 };
 
 // PATCH
+export const PATCH = async (req, { params }) => {
+  const { prompt, tag } = await req.json();
+
+  try {
+    await connectToDB();
+
+    const existingPrompt = await Prompt.findById(params.id);
+
+    if (!existingPrompt)
+      return new Response('Prompt not found', { status: 404 });
+
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    await existingPrompt.save();
+
+    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+  } catch (error) {
+    return new Response('Failed to update prompt', { status: 500 });
+  }
+};
 
 // DELETE
